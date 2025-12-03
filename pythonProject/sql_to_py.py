b6c9569218a28e7c2fcb6c6099172d510924083e
Owen Addison <ofaddison@gmail.com>
2025-12-03 15:02:10 -0600

    added full sql to class covnersion

--- pythonProject/sql_to_py.py
+++ pythonProject/sql_to_py.py
@@ -1,34 +1,55 @@
 
 def parse_sql(sql_file):
     with open(sql_file, "r" ) as sql:
+        output = '@startuml'
         tables = {}
+        final_dict = {}
         in_table = False
-        keywords = {' INT', 'VARCHAR', 'TEXT', 'DECIMAL'}
+        relationships = set()
+        keywords = {' INT': 'Integer', 'VARCHAR': 'String', 'TEXT': 'String', 'DECIMAL': 'Float'}
         for line in sql.readlines():
             curr_kw = ''
             line = line.strip()
             if line and not line.startswith('--'):
-                print(line)
                 if line.startswith('CREATE TABLE'):
                     table_name = line[13:-2]
-                    tables[table_name] = []
+                    tables[table_name] = {}
                     in_table = True
 
                 elif line == ");":
                     in_table = False
                 elif in_table:
-                    for kw in keywords:
+                    for kw in keywords.keys():
                         if kw in line:
                             curr_kw = kw
                             break
                     if curr_kw:
                         index = line.index(curr_kw)
                         value = line[:index].strip()
-                        tables[table_name].append(value)
+                        if value != 'id':
+                            tables[table_name][value] = keywords[curr_kw]
+        for table in tables:
+            final_dict[table] = {}
+            for value in tables[table]:
+                if 'id' in value:
+                    curr_idx = value.index('id')
+                    new_value = value[:curr_idx-1]
+                    final_dict[table][new_value] = new_value
+                else:
+                    final_dict[table][value] = tables[table][value]
 
+        first_table = True
+        for table in final_dict:
+            if first_table:
+                output += f'\n  class {table}' + ' {'
+                first_table = False
+            else:
+                output += '\n    }' + f'\n  class {table}' + ' {'
+            for value in final_dict[table]:
+                output += f'\n       +{final_dict[table][value][0].upper()}{final_dict[table][value][1:]} {value}'
+        if not first_table:
+            output += '\n    }'
+        
+        return output
 
-
-        print(tables)
-
-
-parse_sql("schema.txt")
+print(parse_sql("schema.txt"))
